73ee11066484ded58388ab1268f3eb4fc9000a10
OscarAF <oscaraforsman@gmail.com>
2016-03-06 21:38:16 +0100

    edited template from Kenneth Reitz

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='traderenv',
     version='0.0.1',
-    description='Sample package for Python-Guide.org',
+    description='A trading environment using Nordnet NEXT API',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Oscar Andersson Forsman',
+    author_email='oscaraforsman@gmail.com',
+    url='https://github.com/OscarAF/traderenv',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
